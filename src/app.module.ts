8eb2b8a7a9cd33e054266829c309acdbb111dc32
TheJang <wkddnjs7310@naver.com>
2021-01-08 12:29:14 +0900

    app.module.ts configuring service process.env apply

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,38 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { PodcastsModule } from './podcasts/podcasts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    PodcastsModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+/* ignoreFile 서버에 deploy 할 때 환경변수 파일을 사용하지 않는다는 얘기 */
+
+/* String을 Number로 바꾸는 경우 +를 넣어주면 된다. */
