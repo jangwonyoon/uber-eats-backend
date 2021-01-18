a20257c8fd8ae193e31c7be7ebbe195b989007c4
TheJang <wkddnjs7310@naver.com>
2021-01-18 15:16:47 +0900

    unit testing mocking for TyepScript done

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verfication.entity.dto';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
   });
@@ -19,4 +57,5 @@ describe('UsersService', () => {
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
+  it.todo('verifyEMail');
 });
