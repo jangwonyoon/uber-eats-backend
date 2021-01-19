351756035f1ec3a42d11b46b6807b03fa92518aa
TheJang <wkddnjs7310@naver.com>
2021-01-20 01:20:00 +0900

    [ADD]userProfile e2e testing done

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,7 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { getConnection } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 jest.mock('got', () => {
   return {
@@ -19,6 +21,7 @@ const testUser = {
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let usersRepository: Repository<User>;
   let jwtToken: string;
 
   beforeAll(async () => {
@@ -26,6 +29,7 @@ describe('UserModule (e2e)', () => {
       imports: [AppModule],
     }).compile();
     app = module.createNestApplication();
+    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
     await app.init();
   });
 
@@ -92,16 +96,17 @@ describe('UserModule (e2e)', () => {
         .post(GRAPHQL_ENDPOINT)
         .send({
           query: `
-        mutation {
-          login(input:{
-            email:"${testUser.email}",
-            password:"${testUser.password}",
-          }){
-            ok
-            error
-            token
+          mutation {
+            login(input:{
+              email:"${testUser.email}",
+              password:"${testUser.password}",
+            }) {
+              ok
+              error
+              token
+            }
           }
-        }`,
+        `,
         })
         .expect(200)
         .expect((res) => {
@@ -116,22 +121,22 @@ describe('UserModule (e2e)', () => {
           jwtToken = login.token;
         });
     });
-
-    it(`should not be able to login with wrong credentials`, () => {
+    it('should not be able to login with wrong credentials', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
         .send({
           query: `
-      mutation {
-        login(input:{
-          email:"${testUser.email}",
-          password:"1233",
-        }){
-          ok
-          error
-          token
-        }
-      }`,
+          mutation {
+            login(input:{
+              email:"${testUser.email}",
+              password:"xxx",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `,
         })
         .expect(200)
         .expect((res) => {
@@ -147,7 +152,80 @@ describe('UserModule (e2e)', () => {
     });
   });
 
-  it.todo('userProfile');
+  describe('userProfile', () => {
+    let userId: number;
+    beforeAll(async () => {
+      const [user] = await usersRepository.find();
+      userId = user.id;
+    });
+    it("should see a user's profile", () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+        {
+          userProfile(userId:${userId}) {
+            ok
+            error
+            user {
+              id
+            }
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                userProfile: {
+                  ok,
+                  error,
+                  user: { id },
+                },
+              },
+            },
+          } = res;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+          expect(id).toBe(userId);
+        });
+    });
+    it('should not find a profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+      {
+        userProfile(userId:6666) {
+          ok
+          error
+          user {
+            id
+          }
+        }
+      }
+      `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                userProfile: { ok, error, user },
+              },
+            },
+          } = res;
+          expect(ok).toBe(false);
+          expect(error).toBe('User Not Found');
+          expect(user).toBe(null);
+        });
+    });
+  });
+
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
